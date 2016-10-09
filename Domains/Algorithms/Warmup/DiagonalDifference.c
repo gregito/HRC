ccbf78b57b3e1b05a6a086280956cd6098343bd6
Mészáros Gergely <grigorij9026@gmail.com>
2016-10-10 00:27:11 +0200

    feladatleírás a kódban

--- Domains/Algorithms/Warmup/DiagonalDifference.c
+++ Domains/Algorithms/Warmup/DiagonalDifference.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * összeadjuk az n*n-es mátrix két főátlójának összegét,
+ * majd megmondjuk mennyi a külömbség köztük
+ *
+ */
+
 void printMatrix(int n, int matrix[n][n]) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; ++j) {
